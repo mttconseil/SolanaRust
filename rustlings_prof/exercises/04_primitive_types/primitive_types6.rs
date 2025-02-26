28ddf9759da4145dc3d20ec48c983ac5a722a938
Odomart <odomart69@gmail.com>
2025-02-26 19:33:04 +0100

    primitive type

--- rustlings_prof/exercises/04_primitive_types/primitive_types6.rs
+++ rustlings_prof/exercises/04_primitive_types/primitive_types6.rs
@@ -6,11 +6,14 @@ fn main() {
 mod tests {
     #[test]
     fn indexing_tuple() {
-        let numbers = (1, 2, 3);
+        let numbers = (1, 2, 3); // t.0 t.1 t.2
 
         // TODO: Use a tuple index to access the second element of `numbers`
         // and assign it to a variable called `second`.
-        // let second = ???;
+        //let second: i32 = numbers.1;
+
+        // Même chose
+        //let (premier, second, troisieme) = numbers;
 
         assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
     }
