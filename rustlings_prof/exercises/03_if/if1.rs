49272b93de3f68cd0cef56c27997f9ec0b0ad0f3
Odomart <odomart69@gmail.com>
2025-02-26 18:47:10 +0100

    if exercises

--- rustlings_prof/exercises/03_if/if1.rs
+++ rustlings_prof/exercises/03_if/if1.rs
@@ -4,10 +4,17 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let result: i32 = bigger(10, 8);
+    println!("{}", result)
 }
 
 // Don't mind this for now :)
